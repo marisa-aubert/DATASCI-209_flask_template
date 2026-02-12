7c95cdfc7e0fb16c2b7e5b8664d934390e2631d8
Marisa Aubert <marisa_aubert@berkeley.edu>
2026-02-11 17:21:57 -0800

    flask api

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+import sqlite3
+
+con = sqlite3.connect("players_20.db", check_same_thread=False)
+cur = con.cursor()
 app = Flask(__name__)
 
 @app.route('/')
 def w209():
     file='about9.jpg'
     return render_template('w209.html',file=file)
+
+@app.route('/api')
+def api():
+    return("x": 2)
+
+@app.route("/players/count")
+def getPlayersCount():
+    res = cur.execute("SELECT COUNT(*) as ct FROM users")
+    return ("count" : res.fetchone{}[0])
 
 if __name__ == '__main__':
     app.run()
